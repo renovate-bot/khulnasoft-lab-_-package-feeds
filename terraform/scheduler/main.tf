551cb01458c4593949a37848a21bbaa933fa16ea
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-08-19 05:59:08 +0600

    Create main.tf

---
+++ terraform/scheduler/main.tf
@@ -0,0 +1,44 @@
+resource "google_cloud_scheduler_job" "trigger-ecosystem-scheduler" {
+  name        = "trigger-feeds-scheduler"
+  description = "Scheduler for packages from feeds"
+  schedule    = "*/5 * * * *"
+
+  http_target {
+    http_method = "POST"
+    uri         = google_cloud_run_service.run-scheduler.status[0].url
+
+    oidc_token {
+      service_account_email = var.service-account-email
+    }
+  }
+}
+
+resource "google_cloud_run_service" "run-scheduler" {
+  name     = "scheduled-feeds-srv"
+  location = var.region
+  autogenerate_revision_name = true
+
+  metadata {
+    annotations = {
+      "autoscaling.knative.dev/maxScale" = "1"
+    }
+  }
+  template {
+    spec {
+      container_concurrency = 1
+      containers {
+        image = "gcr.io/${var.project}/scheduled-feeds"
+        env {
+          name  = "OSSMALWARE_TOPIC_URL"
+          value = "gcppubsub://${var.pubsub-topic-feed-id}"
+        }
+        resources {
+          limits = {
+            memory = "2Gi"
+            cpu = "1000m"
+          }
+        }
+      }
+    }
+  }
+}
