e27c291db7d31d80459bad9e244ebba561578f5b
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-08-19 06:02:11 +0600

    Create http_requests.go

---
+++ pkg/utils/http_requests.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var ErrUnsuccessfulRequest = errors.New("unsuccessful request")
+
+func CheckResponseStatus(res *http.Response) error {
+	if res.StatusCode < 200 || res.StatusCode > 299 {
+		return fmt.Errorf("%w: %v", ErrUnsuccessfulRequest, res.Status)
+	}
+	return nil
+}
+
+func IsNotModified(res *http.Response) bool {
+	return res.StatusCode == http.StatusNotModified
+}
