615feb1764ef071e24c89b22bd274fff916f8bf0
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-08-19 06:04:27 +0600

    Create mocks.go

---
+++ pkg/scheduler/mocks.go
@@ -0,0 +1,43 @@
+//nolint:gocritic
+package scheduler
+
+import (
+	"context"
+	"time"
+
+	"github.com/khulnasoft-lab/package-feeds/pkg/feeds"
+)
+
+type mockFeed struct {
+	packages []*feeds.Package
+	errs     []error
+	cutoff   time.Time
+	options  feeds.FeedOptions
+}
+
+func (feed mockFeed) GetName() string {
+	return "mockFeed"
+}
+
+func (feed mockFeed) GetFeedOptions() feeds.FeedOptions {
+	return feed.options
+}
+
+func (feed mockFeed) Latest(cutoff time.Time) ([]*feeds.Package, time.Time, []error) {
+	return feed.packages, feed.cutoff, feed.errs
+}
+
+type mockPublisher struct {
+	sendCallback func(string) error
+}
+
+func (pub mockPublisher) Send(ctx context.Context, body []byte) error {
+	if pub.sendCallback != nil {
+		return pub.sendCallback(string(body))
+	}
+	return nil
+}
+
+func (pub mockPublisher) Name() string {
+	return "mockPublisher"
+}
